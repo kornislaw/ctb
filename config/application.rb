3e20fa0c19504bec9bcae0964c0d6155b435c6ac
Dominik Piekarczyk <piekarczyk@gmail.com>
2012-03-12 14:54:48 +0100

    ok, let's see if this is enough for heroku

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module Blog
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # this is my try - heroku has problems with not precompiled files
+    config.assets.precompile += %w( *.js *.css )
   end
 end
